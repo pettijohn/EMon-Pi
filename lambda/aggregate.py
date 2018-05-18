b66dfc9a41b02a23f70956e3d9ed922fc8f90c9e
pettijohn <pettijohn@gmail.com>
2018-05-17 21:17:14 -0700

    Workign on aggregation

--- lambda/aggregate.py
+++ lambda/aggregate.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from monthdelta import monthdelta
 from typing import List
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 class MockTable:
     def __init__(self, tableName):
@@ -26,41 +27,56 @@ class BucketRule:
     def GetTable(self, tableName):
         return boto3.resource('dynamodb').Table(tableName)
 
-    def __init__(self, tableSuffix: str, bucketFormat: str, aggedFrom: type, *aggedTo):
-        self.TableSuffix = tableSuffix
+    # FIXME - decide on a pattern. Either suffix, format, etc, pass in to ctor. Or child classes override. 
+    def __init__(self, bucketFormat: str, eventTime: datetime, aggedFrom, *aggedTo):
         self.BucketFormat = bucketFormat
         self.AggedFrom = aggedFrom
         self.AggedTo = aggedTo
-    def BucketID(self, time: datetime) -> str:
-        """ ID used in underlying storage """
-        return datetime.strftime(self.BucketFormat)
-    def BucketStartTime(self, time:datetime) -> datetime:
-        return datetime.strptime(self.BucketID(time), self.BucketFormat).replace(tzinfo=timezone.utc)
-    def BucketEndTime(self, time: datetime) -> datetime:
+        self.EventTime = eventTime
+
+    def TableSuffix(self) -> str:
+        """ Table is EnergyMonitor.<TableSuffix> """
         pass
-    def CountInBucket(self, time: datetime) -> int:
+
+    def BucketID(self) -> str:
+        """ ID used in underlying storage """
+        return self.EventTime.strftime(self.BucketFormat)
+    def BucketStartTime(self) -> datetime:
+        return datetime.strptime(self.BucketID(), self.BucketFormat).replace(tzinfo=timezone.utc)
+    def BucketEndTime(self) -> datetime:
+        pass
+    def CountInBucket(self) -> int:
         """ How many of the previous bucket are required to make this bucket? """
         pass
-    def ProcessEvent(self, prevBucket: BucketRule, eventTime: datetime, values: dict):
+    def ProcessEvent(self, values: dict):
         """ Take an event, aggregate it, and call the next bucket(s) """
         # All rules other than Minute follow a standard pattern:
         # - Get the existing row for the bucket, if present
-        # - If an average rule, divide this share and add
-        # - Else if a sum rule, just add
-        # - Save back to table
-        item = self.GetItem(eventTime, values)
+        # - Get all of the rows from the previous bucket that aggregate into this bucket
+        # - Aggregate
+        # - Save to table
+        item = self.GetItem(values)
         insert = False
         if item is None:
-            # Set everything to zero and insert
             insert = True
             item = { 
                 'device_id': values['device_id'],
-                'bucket_id': self.BucketID(eventTime),
+                'bucket_id': self.BucketID(),
                 "current": Decimal(0),
                 "volts": Decimal(0),
                 "watt_hours": Decimal(0),
                 "cost_usd": Decimal(0)
             }
+
+        # Find the start and end bucket IDs for this bucket
+        childStart = self.BucketID
+        childEnd = self.BucketEndTime().strftime(self.BucketFormat)
+        # The child bucket
+        childTable = self.GetTable(self.AggedFrom.TableSuffix())
+        # And all of the constituent rows
+        childRows = childTable.query(
+            KeyConditionExpression=Key('device_id').eq(values['device_id']) & Key('bucket_id').gte(childStart) & Key('bucket_id').lt(childEnd)
+        )
 
         # Update by averaging or summing 
         # FIXME this pattern has bugs. If I insert to the minute bucket and then sum to the hour bucket,
@@ -68,8 +84,8 @@ class BucketRule:
         # Should I add the minute bucket to each bucket?
         # Or recompute each one by selecting everything each time?
         # Selecting and re-agg'ing seems more reliable, but may incure higher DynamoDB costs
-        item['current'] = item['current'] + (values['current'] / Decimal(self.CountInBucket))
-        item['volts'] = item['volts'] + (values['volts'] / Decimal(self.CountInBucket))
+        item['current'] = item['current'] + (values['current'] / Decimal(self.CountInBucket()))
+        item['volts'] = item['volts'] + (values['volts'] / Decimal(self.CountInBucket()))
         item['watt_hours'] = item['watt_hours'] + values['watt_hours']
         item['cost_usd'] = item['cost_usd'] + values['cost_usd']
 
@@ -94,15 +110,15 @@ class BucketRule:
 
         # Chain and call the next level of buckets in the hierarchy
         for to in self.AggedTo:
-            nextBucket = to()
+            nextBucket = to(self.EventTime) # FIXME need other parameters, and may need to extract bucket chaining from this
             # FIXME - passing item to the next level is wrong
-            nextBucket.ProcessEvent(self, eventTime, item)
+            nextBucket.ProcessEvent(self, item) # FIXME - pass in updated item or original values?
         
-    def GetItem(self, eventTime: datetime, values: dict) -> dict:
+    def GetItem(self, values: dict) -> dict:
         """ Returns the item from the this bucket table """
         table = self.GetTable("EnergyMonitor." + self.TableSuffix)
         
-        values['bucket_id'] = self.BucketID(eventTime)
+        values['bucket_id'] = self.BucketID()
         response = table.get_item(
             Key={
                 'device_id': values['device_id'],
@@ -115,16 +131,19 @@ class BucketRule:
             return None
 
 class MinuteBucket(BucketRule):
-    def __init__(self):
-        super().__init__("Minute", "%Y-%m-%dT%H:%MZ", None, HourBucket)
+    def __init__(self, eventTime: datetime):
+        super().__init__("%Y-%m-%dT%H:%MZ", eventTime, None, HourBucket)
 
-    def BucketEndTime(self, time:datetime) -> datetime:
-        return self.BucketStartTime(time) + timedelta(minutes=1)
+    def TableSuffix(self):
+        return "Minute"        
+
+    def BucketEndTime(self) -> datetime:
+        return self.BucketStartTime() + timedelta(minutes=1)
     
-    def CountInBucket(self, time: datetime) -> int:
-        return 1 #(self.BucketEndTime(time) - self.BucketStartTime(time)).total_seconds()
+    def CountInBucket(self) -> int:
+        return 1 
 
-    def ProcessEvent(self, prevBucket: BucketRule, eventTime: datetime, values: dict):
+    def ProcessEvent(self, values: dict):
         item = self.GetItem(values)
         if item is not None:
             # Got a matching row
@@ -137,65 +156,63 @@ class MinuteBucket(BucketRule):
         # Chain and call the next level of buckets in the hierarchy
         for to in self.AggedTo:
             nextBucket = to()
-            nextBucket.ProcessEvent(self, eventTime, values)
+            nextBucket.ProcessEvent(self, values)
 
 class HourBucket(BucketRule):
-    def __init__(self):
-        super().__init__("%Y-%m-%dT%H:00Z", MinuteBucket, DayBucket)
+    def __init__(self, eventTime: datetime):
+        super().__init__("%Y-%m-%dT%H:00Z", eventTime, MinuteBucket, DayBucket)
 
-    def BucketEndTime(self, time:datetime) -> datetime:
-        return self.BucketStartTime(time) + timedelta(hours=1)
+    def TableSuffix(self):
+        return "Hour"
+
+    def BucketEndTime(self) -> datetime:
+        return self.BucketStartTime() + timedelta(hours=1)
     
-    def CountInBucket(self, time: datetime) -> int:
-        return (self.BucketEndTime(time) - self.BucketStartTime(time)).total_seconds()/60
+    def CountInBucket(self) -> int:
+        return (self.BucketEndTime() - self.BucketStartTime()).total_seconds()/60
 
     def ProcessEvent(self, prevBucket: BucketRule, eventTime: datetime, values: dict):
         pass
 
 
 class DayBucket(BucketRule):
-    def __init__(self):
-        super().__init__("%Y-%m-%dT00:00Z", HourBucket)
+    def __init__(self, eventTime: datetime):
+        super().__init__("%Y-%m-%dT00:00Z", eventTime, HourBucket, MonthBucket)
 
-    def BucketEndTime(self, time:datetime) -> datetime:
-        return self.BucketStartTime(time) + timedelta(days=1)
+    def TableSuffix(self):
+        return "Day"
+
+    def BucketEndTime(self) -> datetime:
+        return self.BucketStartTime() + timedelta(days=1)
     
-    def CountInBucket(self, time: datetime) -> int:
-        return (self.BucketEndTime(time) - self.BucketStartTime(time)).total_seconds()/3600
+    def CountInBucket(self) -> int:
+        return (self.BucketEndTime() - self.BucketStartTime()).total_seconds()/3600
 
 class MonthBucket(BucketRule):
-    def __init__(self):
-        super().__init__("%Y-%m-01T00:00Z", DayBucket)
+    def __init__(self, eventTime: datetime):
+        super().__init__("%Y-%m-01T00:00Z", eventTime, DayBucket, YearBucket)
 
-    def BucketEndTime(self, time:datetime) -> datetime:
-        return self.BucketStartTime(time) + monthdelta(1)
+    def TableSuffix(self):
+        return "Month"
+
+    def BucketEndTime(self) -> datetime:
+        return self.BucketStartTime() + monthdelta(1)
     
-    def CountInBucket(self, time: datetime) -> int:
-        return (self.BucketEndTime(time) - self.BucketStartTime(time)).days
+    def CountInBucket(self) -> int:
+        return (self.BucketEndTime() - self.BucketStartTime()).days
 
 class YearBucket(BucketRule):
-    def __init__(self):
-        super().__init__("%Y-01-01T00:00Z", DayBucket)
+    def __init__(self, eventTime: datetime):
+        super().__init__("%Y-01-01T00:00Z", eventTime, MonthBucket, None)
 
-    def BucketEndTime(self, time:datetime) -> datetime:
-        return self.BucketStartTime(time).replace(year=self.BucketStartTime(time).year+1)
+    def TableSuffix(self):
+        return "Year"
+
+    def BucketEndTime(self) -> datetime:
+        return self.BucketStartTime().replace(year=self.BucketStartTime().year+1)
     
-    def CountInBucket(self, time: datetime) -> int:
-        return (self.BucketEndTime(time) - self.BucketStartTime(time)).days
-
-class AllBuckets:
-    MinuteBucket = MinuteBucket()
-    HourBucket = HourBucket()
-    DayBucket = DayBucket()
-    MonthBucket = MonthBucket()
-    YearBucket = YearBucket()
-    All = [MinuteBucket, HourBucket, DayBucket, MonthBucket, YearBucket]
-
-
-
-class AllTables:
-    Minute = Table('EnergyMonitor.Minute', MinuteBucket(), AggField("device_id", ))
-
+    def CountInBucket(self) -> int:
+        return (self.BucketEndTime() - self.BucketStartTime()).days
 
 
 # Tests
@@ -203,34 +220,43 @@ if (__name__ == "__main__"):
     baseCase     = datetime(2018,5,5,13,39,12, tzinfo=timezone.utc)
     leapyearCase = datetime(2016,2,29,13,39,12, tzinfo=timezone.utc)
     dstStartCase = datetime(2018,3,11,2,39,12, tzinfo=timezone.utc) # No effect, since UTC
+    
+    
+    mb = MinuteBucket(baseCase)
+    assert mb.BucketID() == "2018-05-05T13:39Z"
+    assert mb.BucketEndTime() == datetime(2018,5,5,13,40, tzinfo=timezone.utc)
+    assert mb.CountInBucket() == 1
 
-    assert AllBuckets.MinuteBucket.BucketID(baseCase) == "2018-05-05T13:39Z"
-    assert AllBuckets.MinuteBucket.BucketEndTime(baseCase) == datetime(2018,5,5,13,40, tzinfo=timezone.utc)
-    assert AllBuckets.MinuteBucket.CountInBucket(baseCase) == 1
+    hb = HourBucket(baseCase)
+    assert hb.BucketID() == "2018-05-05T13:00Z"
+    assert hb.BucketEndTime() == datetime(2018,5,5,14,0, tzinfo=timezone.utc)
+    assert hb.CountInBucket() == 60
 
-    assert AllBuckets.HourBucket.BucketID(baseCase) == "2018-05-05T13:00Z"
-    assert AllBuckets.HourBucket.BucketEndTime(baseCase) == datetime(2018,5,5,14,0, tzinfo=timezone.utc)
-    assert AllBuckets.HourBucket.CountInBucket(baseCase) == 60
+    db = DayBucket(baseCase)
+    assert db.BucketID() == "2018-05-05T00:00Z"
+    assert db.BucketEndTime() == datetime(2018,5,6,0,0, tzinfo=timezone.utc)
+    assert db.CountInBucket() == 24
 
-    assert AllBuckets.DayBucket.BucketID(baseCase) == "2018-05-05T00:00Z"
-    assert AllBuckets.DayBucket.BucketEndTime(baseCase) == datetime(2018,5,6,0,0, tzinfo=timezone.utc)
-    assert AllBuckets.DayBucket.CountInBucket(baseCase) == 24
+    mt = MonthBucket(baseCase)    
+    assert mt.BucketID() == "2018-05-01T00:00Z"
+    assert mt.BucketEndTime() == datetime(2018,6,1,0,0, tzinfo=timezone.utc)
+    assert mt.CountInBucket() == 31
 
-    assert AllBuckets.MonthBucket.BucketID(baseCase) == "2018-05-01T00:00Z"
-    assert AllBuckets.MonthBucket.BucketEndTime(baseCase) == datetime(2018,6,1,0,0, tzinfo=timezone.utc)
-    assert AllBuckets.MonthBucket.CountInBucket(baseCase) == 31
+    yb = YearBucket(baseCase)
+    assert yb.BucketID() == "2018-01-01T00:00Z"
+    assert yb.BucketEndTime() == datetime(2019,1,1,0,0, tzinfo=timezone.utc)
+    assert yb.CountInBucket() == 365
 
-    assert AllBuckets.YearBucket.BucketID(baseCase) == "2018-01-01T00:00Z"
-    assert AllBuckets.YearBucket.BucketEndTime(baseCase) == datetime(2019,1,1,0,0, tzinfo=timezone.utc)
-    assert AllBuckets.YearBucket.CountInBucket(baseCase) == 365
+    ml = MonthBucket(leapyearCase)
+    assert ml.BucketID() == "2016-02-01T00:00Z"
+    assert ml.BucketEndTime() == datetime(2016,3,1,0,0, tzinfo=timezone.utc)
+    assert ml.CountInBucket() == 29
 
-    assert AllBuckets.MonthBucket.BucketID(leapyearCase) == "2016-02-01T00:00Z"
-    assert AllBuckets.MonthBucket.BucketEndTime(leapyearCase) == datetime(2016,3,1,0,0, tzinfo=timezone.utc)
-    assert AllBuckets.MonthBucket.CountInBucket(leapyearCase) == 29
-
-    assert AllBuckets.YearBucket.BucketID(leapyearCase) == "2016-01-01T00:00Z"
-    assert AllBuckets.YearBucket.BucketEndTime(leapyearCase) == datetime(2017,1,1,0,0, tzinfo=timezone.utc)
-    assert AllBuckets.YearBucket.CountInBucket(leapyearCase) == 366
+    yl = YearBucket(leapyearCase)
+    assert yl.BucketID() == "2016-01-01T00:00Z"
+    assert yl.BucketEndTime() == datetime(2017,1,1,0,0, tzinfo=timezone.utc)
+    assert yl.CountInBucket() == 366
 
     # Override the dynamo table with mock for testing
     BucketRule.GetTable = lambda self, tableName: MockTable(tableName)
+    # TODO define bucket chaining rule somehow
